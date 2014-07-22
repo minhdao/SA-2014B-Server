fb341a75034cae14c752aeea0ef287691c7868ab
Minh Dao <minhdao6@gmail.com>
2014-07-22 13:01:11 +0700

    added description and fields into Status

--- src/main/java/Status.java
+++ src/main/java/Status.java
@@ -1,6 +1,14 @@
 /**
  * Created by minh on 7/22/14.
+ * Only one player can win a game
+ * Lose will be set for all other players
+ * Continue indicates that the game can go on
+ * Only one player is allowed to move at a time
+ * Wait indicate that another player is playing
  */
 public enum Status {
-    
+    Win,
+    Lose,
+    Wait,
+    Continue
 }
